5d4b3222a465197b22ce189d4b8fc640eb0a5702
gedimitrova <37780038+gedimitrova@users.noreply.github.com>
2018-04-02 17:02:15 +0300

    Add files via upload

---
+++ Problem1.700829.R
@@ -0,0 +1,28 @@
+# a)
+x <- c(4,1,1,4)
+x
+# b)
+y <- c(1,4)
+y
+# c)
+x-y
+
+# d)
+s <- c(x,y)
+s
+# e)
+rep(s,10)
+srep <- rep(s,10)
+length(srep)
+# f)
+rep(s,each=3)
+# g)
+seq(7,21, by=1)
+7:21
+# h)
+iseq <- seq(7,21, by=1)
+length(iseq)
+
+
+
+
